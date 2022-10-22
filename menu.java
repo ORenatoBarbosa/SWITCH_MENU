8e6d172aeb474d91c7e3a205e5a06b79fed54459
Renato Barbosa <98436200+ORenatoBarbosa@users.noreply.github.com>
2022-10-22 17:10:24 -0300

    Update and rename .gitignore to menu.java

---
+++ menu.java
@@ -0,0 +1,39 @@
+package aulas;
+
+import java.util.Scanner;
+
+public class Aula_16 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int codigo;
+		
+		System.out.println("digite uma opçao de 1 a 5:");
+		codigo = sc.nextInt();
+		
+		switch (codigo) {
+		case 1:
+			System.out.println("cachorro quente");
+			System.out.println("R$ 4.00");
+			break;
+		case 2:
+			System.out.println("X-salada");
+			System.out.println("R$ 4.50");
+			break;
+		case 3:
+			System.out.println("X-Bacon");
+			System.out.println("R$ 5.00");
+			break;
+		case 4:
+			System.out.println("Torrada Simples");
+			System.out.println("R$ 2.00");
+			break;
+		case 5:
+			System.out.println("Refrigerante");
+			System.out.println("R$ 1.50");
+			break;
+		default:
+			System.out.println("Opção Invalida");
+		}
+	}	
+}
